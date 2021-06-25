6a3cfddb7cd24519da2a59402e74fee8a84c345f
전형민 <jpro6679@naver.com>
2021-06-25 13:21:10 +0900

    Updates

---
+++ component---src-pages-info-tsx-da06c3085b7a28752bf6.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[977],{3051:function(t,a,e){"use strict";e.r(a);e(7294);var s=e(3431);var r={name:"t0gkck",styles:"*{margin:10px;padding:0;box-sizing:border-box;font-size:20px;}"};a.default=function(t){var a=t.data.site.siteMetadata,e=a.title,i=a.description,n=a.author;return(0,s.tZ)("div",null,(0,s.tZ)(s.xB,{styles:r}),e," ",i," ",n)}}}]);
+//# sourceMappingURL=component---src-pages-info-tsx-da06c3085b7a28752bf6.js.map
